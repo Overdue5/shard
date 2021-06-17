4b95de96d15d10f9474b00af46fc49c9d01c6807
LizzyBELL <85939986+LizzyBELL420@users.noreply.github.com>
2021-06-16 18:04:42 -0700

    Added Elven's Bow and to Craft Menu

---
+++ Scripts/Custom/Adds/Items/Weapons/ElvenBow.cs
@@ -0,0 +1,43 @@
+namespace Server.Items
+{
+    public class ElvenBow : Bow
+    {
+        [Constructable]
+        public ElvenBow()
+        {
+            Hue = 1941;
+            Name = "Elven's Bow";
+            Speed = 510;
+            MinDamage = 8;
+            MaxDamage = 23;
+            AccuracyLevel = WeaponAccuracyLevel.Supremely;
+            DamageLevel = WeaponDamageLevel.Power;
+            DurabilityLevel = WeaponDurabilityLevel.Substantial;
+            IsRenamed = true;
+        }
+
+        public SerpentsTongue(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override void OnSingleClick(Mobile from)
+        {
+            LabelTo(from, Sphere.ComputeCustomWeaponName(this));
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write(0);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+        }
+    }
+}
